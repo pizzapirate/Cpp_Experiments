7d0e1864fa27327b202e5779f09825604e1db0c2
Miles Vellozzo <mvellozzo@hotmail.com>
2023-03-10 16:24:17 +0000

    Experimented using namespaces for global linked variables.

---
+++ Cpp_Experiments/FoodConstants.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+namespace food {
+	const std::string pizza{ "pizza" };
+	const std::string burger{ "burger" };
+}
+
+extern std::string poptarts{ "poptarts - yummy" };
--- Cpp_Experiments/Main.cpp
+++ Cpp_Experiments/Main.cpp
@@ -4,13 +4,18 @@
 #include "MyConsoleLogger.h"
 #include "PrimeNumbers.h"
 #include "RandomNumberGenerator.h"
+#include "FoodConstants.h"
 int main() {
+	
+	extern std::string poptarts;
+	std::cout << poptarts;
+	
 	//PrimeNumberProgram png;
 	//png.Begin();
-	while (true) {
-		randomInt();
-		std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(1));
-	}
+	//while (true) {
+	//	randomInt();
+	//	std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(1));
+	//}
 
 	return 0;
 }
